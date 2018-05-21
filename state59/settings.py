2824acba154ec9086b44453f8ee4c36f22c693df
lmerengues <317958662@qq.com>
2018-05-21 23:04:57 +0800

    更改settings

--- state59/settings.py
+++ state59/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'state59.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'test',
+        'PASSWORD': 'test123',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
--- state59/view.py
+++ state59/view.py
@@ -4,7 +4,19 @@ import json
 from django.db import connections
 
 def index(request):
+
     hno = request.GET['hno']
+
+
+
     raw = {"id":1,"text":"xxx"}
+
+
+
+
+
     response = HttpResponse(json.dumps(raw), content_type="application/json")
     return response
+
+
+

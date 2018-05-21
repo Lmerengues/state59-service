c0c367e132bdfa262f178a0708edc2ac689316af
lmerengues <317958662@qq.com>
2018-05-21 23:07:51 +0800

    加入查询数据库

--- state59/view.py
+++ state59/view.py
@@ -3,6 +3,14 @@ from django.http import HttpResponse
 import json
 from django.db import connections
 
+def dictfetchall(cursor):
+	desc = cursor.description
+	return [
+	dict(zip([col[0] for col in desc], row))
+    	for row in cursor.fetchall()
+    	]
+
+
 def index(request):
 
     hno = request.GET['hno']
@@ -11,6 +19,11 @@ def index(request):
 
     raw = {"id":1,"text":"xxx"}
 
+    cursor = connections['default'].cursor()
+    cursor.execute("select * from homework limit 10")
+
+    raw = dictfetchall(cursor)
+    cursor.close()
 
 
 

8d1b2a68cb0cfd4cfbac281aab77b365fcb25c4e
root <root@localhost.localdomain>
2018-06-07 20:02:03 +0800

    onlien version

--- state59/editprofile.py
+++ state59/editprofile.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 from django.http import HttpResponse
 
 import json
@@ -19,6 +20,13 @@ def index(request):
     cursor.execute("select unickName, ugender, uavatarurl from Users where uid = %s", (uid,))
     raw = dictfetchall(cursor)
     cursor.close()
+    for data in raw:
+        if data['ugender'] == 0:
+            data['ugender_text'] = '未知'
+        elif data['finished_label'] == 1:
+            data['ugender_text'] = '男'
+        elif data['finished_label'] == 2:
+            data['ugender_text'] = '女'
 
     response = HttpResponse(json.dumps(raw), content_type="application/json")
     return response

a87630d3453e4596112e3ec9e4ae64b42887d5aa
blake <2015201974@ruc.edu.cn>
2018-06-07 23:24:51 +0800

    detail

--- state59/frontpage.py
+++ state59/frontpage.py
@@ -60,7 +60,7 @@ def detail(request):
 
     newraw = raw[0]
     if newraw["ismoney"] == 1:
-        newraw["hmoney"] = float(newraw[hmoney]) / 100
+        newraw["hmoney"] = float(newraw["hmoney"]) / 100
     newraw["hddl"] = datetime.strftime(newraw["hddl"],"%Y-%m-%d %H:%M:%S")
     newraw["hpublic"] = datetime.strftime(newraw["hpublic"],"%Y-%m-%d %H:%M:%S")
     response = HttpResponse(json.dumps(newraw), content_type="application/json")

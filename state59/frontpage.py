9d61f33595b68a8e0acbc53992bd1ac02ec34227
blake <2015201974@ruc.edu.cn>
2018-06-07 19:55:20 +0800

    ..

--- state59/frontpage.py
+++ state59/frontpage.py
@@ -5,12 +5,23 @@ from django.db import connections
 from datetime import datetime
 
 def dictfetchall(cursor):
-    desc = cursor.description
-    return [
-    dict(zip([col[0] for col in desc], row))
-        for row in cursor.fetchall()
-        ]
+	desc = cursor.description
+	return [
+	dict(zip([col[0] for col in desc], row))
+    	for row in cursor.fetchall()
+    	]
 
+def getsecond(date):
+
+    now = datetime.now()
+    day_min = date.split()
+    year = day_min[0].split('-')[0]
+    month = day_min[0].split('-')[1]
+    day = day_min[0].split('-')[2]
+    hour = day_min[1].split(':')[0]
+    minute = day_min[1].split(':')[1]
+    dt = datetime(int(year), int(month), int(day), int(hour), int(minute))
+    return (dt - now).seconds
 
 def index(request):
 
@@ -20,8 +31,9 @@ def index(request):
     raw = dictfetchall(cursor)
     cursor.close()
 
+    for record in raw:
+        record["left"] = getsecond(record["tddl"])
+    raw = sort(raw, key=lambda x:x["left"])
+
     response = HttpResponse(json.dumps(raw), content_type="application/json")
     return response
-
-
-
